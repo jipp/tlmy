686ea12a7acbb380bf26d792c9181a5e1f92fa2a
jipp <wolfgang.keller@wobilix.de>
2017-08-05 22:25:21 +0200

    remove gtune, minor changes

--- tlmy.lua
+++ tlmy.lua
@@ -1,16 +1,16 @@
 --[[
   tlmy.lua
-  
+
   Date: 27.07.2016
   Author: wolfgang.keller@wobilix.de
   HW: FrSky Taranis X9D Plus
-  SW: OpenTX 2.1.8
+  SW: OpenTX 2.1.8; OpenTX 2.2.0
 ]]--
 
 ----------------------------------------------------------------------
 -- Version String
 ----------------------------------------------------------------------
-local version = "v0.14.1"
+local version = "v0.15.0"
 
 ----------------------------------------------------------------------
 -- Mathematical Utility Function
@@ -18,14 +18,14 @@ local version = "v0.14.1"
 local function Round(value, decimal)
   local exponent = 10^(decimal or 0)
   return math.floor(value * exponent + 0.5) / exponent
-end  
+end
 
 ----------------------------------------------------------------------
 -- Wrapper
 ----------------------------------------------------------------------
 local function ShowValue(x, y, key, font)
   local value = getValue(key)
-  
+
   if value ~= nil then
     lcd.drawText(x, y, key .. ": ", font)
     lcd.drawChannel(lcd.getLastPos(), y, key, font + LEFT)
@@ -34,7 +34,7 @@ end
 
 local function ShowTimer(x, y, key, font)
   local value = getValue(key)
-  
+
   if value ~= nil then
     lcd.drawText(x, y, key .. ": ", font)
     lcd.drawTimer(lcd.getLastPos(), y, value, font)
@@ -43,13 +43,13 @@ end
 
 local function ShowGauge(x, y, w, h, key)
   local value = getValue(key.key)
-  
+
   if value ~= nil then
     if type(key.factor) == "function" then
       factor = key.factor()
     else
       factor = key.factor
-    end  
+    end
     if value >= key.max * factor then
       lcd.drawFilledRectangle(x, y, w, h, SOLID)
     elseif value >= key.min * factor then
@@ -58,7 +58,7 @@ local function ShowGauge(x, y, w, h, key)
       lcd.drawRectangle(x, y, w, h)
     end
   end
-end 
+end
 
 ----------------------------------------------------------------------
 -- Classes
@@ -70,7 +70,7 @@ local gauge = {}
   gauge["max"] = 100
   gauge["factor"] = 1
   gauge["smooth"] = 1
- 
+
 function gauge:New(o)
   o = o or {}
   setmetatable(o, self)
@@ -85,18 +85,18 @@ local diagram = {}
   diagram["time"] = 0
   diagram["delta"] = 1
   diagram["extreme"] = 500
-  
+
 function diagram:New(o)
   o = o or {}
   setmetatable(o, self)
   self.__index = self
   return o
 end
-    
+
 function diagram:Add()
   local value = getValue(self.key)
   local diff = 0
-  
+
   if value ~= nil then
     if getTime() - self.time > self.delta * 100 then
       self.time = getTime()
@@ -107,14 +107,14 @@ function diagram:Add()
           self[index] = nil
         end
       end
-	  
+
       if (self[2] ~= nil) and (math.abs(value - self[2]) > self["extreme"]) then
         self[1] = self[2]
       else
         self[1] = value
-      end  
-	  
-    end 
+      end
+
+    end
   end
 end
 
@@ -136,7 +136,7 @@ end
 function diagram:Show(x, y, h)
   local min, max = self:MinMax()
   local diff = 0
-  
+
   lcd.drawText(x + 3, y, self.key .. " " .. Round(self[1], 2) .. "/" .. Round(max, 2) .. "/" .. Round(min, 2), SMLSIZE)
 
   if min > 0 then
@@ -154,25 +154,25 @@ function diagram:Show(x, y, h)
       lcd.drawPoint(x + index, y + h * (max - self[index]) / diff, SOLID, FORCE)
     end
   end
-  
+
   lcd.drawLine(x, y, x, y + h, SOLID, FORCE)
   lcd.drawLine(x - 1, y + 1, x + 1, y + 1, SOLID, FORCE)
-	
+
   if diff ~= 0 then
     lcd.drawLine(x, y + h * max / diff, x + self.length, y + h * max / diff, SOLID, FORCE)
     lcd.drawLine(x + self.length - 1, y + h * max / diff - 1, x + self.length - 1, y + h * max / diff + 1, SOLID, FORCE)
   else
     lcd.drawLine(x, y + h, x + self.length, y + h, SOLID, FORCE)
     lcd.drawLine(x + self.length - 1, y + h - 1, x + self.length - 1, y + h + 1, SOLID, FORCE)
-  end    
-  
+  end
+
   lcd.drawText(x + self.length - 25, y + h + 2 , "I=" .. self["delta"] .. "s", SMLSIZE)
 end
 
 -- Switch
 local switch = {}
   switch["key"] = "key"
- 
+
 function switch:New(o)
   o = o or {}
   setmetatable(o, self)
@@ -182,8 +182,8 @@ end
 
 function switch:GetName()
   local value = getValue(self.key)
-  
-  if value ~= nil then 
+
+  if value ~= nil then
     for index, data in ipairs(self) do
       if data.position == value then
         return data.name
@@ -194,7 +194,7 @@ end
 
 function switch:Show(x, y, font)
   local value = self:GetName()
-   
+
   if value ~= nil then
     lcd.drawText(x, y, value, font)
   end
@@ -203,10 +203,10 @@ end
 -- LiPo
 local lipo = {}
   lipo["key"] = "key"
-  lipo["min"] = 3.2
+  lipo["min"] = 3.3
   lipo["max"] = 4.3
   lipo["cels"] = 3
-   
+
 function lipo:New(o)
   o = o or {}
   setmetatable(o, self)
@@ -252,7 +252,7 @@ end
 local correction = {}
    correction["key"] = "key"
    correction["factor"] = 0
-  
+
 function correction:New(o)
   o = o or {}
   setmetatable(o, self)
@@ -262,7 +262,7 @@ end
 
 function correction:Show(x, y, font)
   local value = getValue(self.key)
-  
+
   if value ~= nil then
     lcd.drawText(x, y, self.key .. ": ", font)
     lcd.drawNumber(lcd.getLastPos(), y, (value - self.factor) * 100, font+PREC2+LEFT)
@@ -272,48 +272,43 @@ end
 
 function correction:Reset()
   local value = getValue(self.key)
-  
+
   if value ~= nil then
     self.factor = value
-  end  
+  end
 end
 
 ----------------------------------------------------------------------
 -- local definitions
 ----------------------------------------------------------------------
-local energy = lipo:New{ key = "VFAS", 
-  { limit = 3.3, delta = 10, file = "batcrit.wav" }, 
+local energy = lipo:New{ key = "VFAS",
+  { limit = 3.3, delta = 10, file = "batcrit.wav" },
   { limit = 3.5, delta = 10, file = "batlow.wav" }
 }
 local rssiGauge = gauge:New{ key = "RSSI", min = 40, max = 100 }
 local vfasGauge = gauge:New{ key = "VFAS", min = energy.min, max =  energy.max, factor = function () return energy.cels end, smooth = 100 }
 local altDiagram = diagram:New{ key = "Alt", delta = 1, extreme = 100 }
-local ch6 = switch:New{ key = "ch6", 
-  { position = -1024, }, 
+local ch6 = switch:New{ key = "ch6",
+  { position = -1024, },
   { position = 0, name = "baro" },
-  { position = 1024 } 
+  { position = 1024 }
 }
-local ch7 = switch:New{ key = "ch7", 
+local ch7 = switch:New{ key = "ch7",
   { position = -1024 },
   { position = 0, name = "osd sw" },
   { position = 1024, name = "air mode" }
 }
-local ch8 = switch:New{ key = "ch8", 
+local ch8 = switch:New{ key = "ch8",
   { position = -1024 },
   { position = 0, name = "beeper" },
   { position = 1024 },
 }
-local ch9 = switch:New{ key = "ch9", 
+local ch9 = switch:New{ key = "ch9",
   { position = -1024 },
   { position = 0 },
   { position = 1024 },
 }
-local ch11 = switch:New{ key = "ch11", 
-  { position = -1024 },
-  { position = 0, name = "gtune" },
-  { position = 1024 }
-}
-local ch13 = switch:New{ key = "ch13", 
+local ch13 = switch:New{ key = "ch13",
   { position = 1024, name = "armed" },
   { position = -1024 }
 }
@@ -324,12 +319,12 @@ local heading = correction:New{ key = "Hdg" }
 ----------------------------------------------------------------------
 local display = {}
   display["width"] = 212
-  display["height"] = 64 
+  display["height"] = 64
 
-function display:Show(screen) 
+function display:Show(screen)
   local flightMode = ( { getFlightMode() } )[2]
   local modelName = model.getInfo().name
-      
+
   lcd.drawScreenTitle(modelName .. "  (" .. energy.cels .. "S)  " .. flightMode .. " - " .. version, screen.num, #screen)
   screen[screen.num]()
 end
@@ -339,7 +334,7 @@ end
 ----------------------------------------------------------------------
 local screen = {}
   screen["num"] = 1
-  
+
 function screen:Next()
   self.num = self.num + 1
   if self.num > #self then
@@ -354,7 +349,7 @@ function screen:Previous()
   end
 end
 
-screen[1] = function() 
+screen[1] = function()
   ShowValue(1, 9, "VFAS", MIDSIZE)
   ShowGauge(107, 9, 100, 12, vfasGauge)
   ShowValue(1, 25, "RSSI", MIDSIZE)
@@ -366,10 +361,9 @@ screen[1] = function()
   ch7:Show(1 + display["width"] * 1 / 5, 56, SMLSIZE)
   ch8:Show(1 + display["width"] * 2 / 5, 56, SMLSIZE)
   ch9:Show(1 + display["width"] * 3 / 5, 56, SMLSIZE)
-  ch11:Show(1 + display["width"] * 4 / 5, 56, SMLSIZE)
 end
 
-screen[2] = function() 
+screen[2] = function()
   ShowValue(1, 9, "Alt", MIDSIZE)
   altDiagram:Show(107, 9, 38)
   ch13:Show(1, 41, MIDSIZE+INVERS+BLINK)
@@ -377,12 +371,11 @@ screen[2] = function()
   ch7:Show(1 + display["width"] * 1 / 5, 56, SMLSIZE)
   ch8:Show(1 + display["width"] * 2 / 5, 56, SMLSIZE)
   ch9:Show(1 + display["width"] * 3 / 5, 56, SMLSIZE)
-  ch11:Show(1 + display["width"] * 4 / 5, 56, SMLSIZE)
 end
 
 ----------------------------------------------------------------------
 local function init_func()
-  -- init_func is called once when model is loaded   
+  -- init_func is called once when model is loaded
 end
 
 local function bg_func()
@@ -394,29 +387,29 @@ end
 local function run_func(event)
   -- run_func is called periodically when screen is visible
   bg_func() -- run typically calls bg_func to start
-    
+
   lcd.clear()
-    
+
   if event == EVT_PLUS_BREAK then
     energy:Increment()
   end
-  
+
   if event == EVT_MINUS_BREAK then
     energy:Decrement()
   end
-  
+
   if event == EVT_PAGE_BREAK then
     screen:Next()
   end
-  
+
   if event == EVT_PAGE_LONG then
     screen:Previous()
   end
-  
+
   if event == EVT_ENTER_BREAK then
     heading:Reset()
   end
-    
+
   display:Show(screen)
 end
 
